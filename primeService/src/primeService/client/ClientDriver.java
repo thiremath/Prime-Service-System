87ee06ce015768fb1f35e39490e41f97d502b23b
Chaitanya Jha <chaitanyajha@Chaitanyas-MacBook-Pro.local>
2023-11-25 19:59:43 -0500

    Implemeneted Client Menu

--- primeService/src/primeService/client/ClientDriver.java
+++ primeService/src/primeService/client/ClientDriver.java
@@ -1,5 +1,91 @@
 package primeService.client;
-
+ 
+// A Java program for a Client
+import java.io.*;
+import java.net.*;
+ 
+ 
 public class ClientDriver {
-    
+    // initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+ 
+    // constructor to put ip address and port
+    public ClientDriver(String ipAddress, int port){
+        ClientMenu clientMenu = new ClientMenu(this) ;
+        // establish a connection
+        try {
+            socket = new Socket(ipAddress, port);
+            System.out.println("Connected");
+ 
+            // takes input from terminal
+            // input = new DataInputStream(System.in);
+ 
+            // // sends output to the socket
+            // out = new DataOutputStream(
+            //  socket.getOutputStream());
+ 
+            clientMenu.processClientMenu();
+ 
+        }
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+ 
+        // string to read message from input
+        String line = "";
+ 
+        // keep reading until "Over" is input
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            }
+            catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+ 
+        // close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+ 
+    }
+ 
+    public void setClientName(){
+            
+    }
+ 
+    public void setClientNumber(){
+ 
+    }
+ 
+    public void getServerResponse(){
+        out.writeUTF("");
+    }
+ 
+    public void quit(){
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+ 
 }
+ 
--- primeService/src/primeService/client/ClientMenu.java
+++ primeService/src/primeService/client/ClientMenu.java
@@ -1,5 +1,46 @@
 package primeService.client;
-
+ 
+import java.util.Scanner;
+ 
 public class ClientMenu {
-    
+ 
+    public ClientDriver clientDriver ;
+    // public String choice ;
+ 
+    public ClientMenu(ClientDriver clientDriverIn){
+        clientDriver = clientDriverIn ;
+        // choice = null ;
+    }
+ 
+    public void processClientMenu(String choice){
+        Scanner scanner = new Scanner(System.in);
+            displayClientMenu();
+            switch(choice){
+                case "1":
+                    clientDriver.setClientName() ;
+                    break ;
+                case "2":
+                    clientDriver.setClientNumber() ;
+                    break ;
+                case "3":
+                    clientDriver.getServerResponse() ;
+                    break ;
+                case "4":
+                    clientDriver.quit() ;
+                    break ;
+                default:
+                    System.err.println("Invalid choice.");
+                    System.exit(0) ;
+            }
+ 
+ 
+    }
+ 
+    public void displayClientMenu(){
+        System.out.println("[1] Set client name");
+        System.out.println("[2] Enter number to query for prime");
+        System.out.println("[3] What is the server response?");
+        System.out.println("[4] Quit");
+    }
 }
+ 
